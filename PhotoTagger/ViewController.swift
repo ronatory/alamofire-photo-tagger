07db241fcca666ef575b656fa95e25789badf247
ronatory <ronatory@gmail.com>
2016-10-13 12:17:20 +0200

    First commit

---
+++ PhotoTagger/ViewController.swift
@@ -0,0 +1,96 @@
+/*
+* Copyright (c) 2015 Razeware LLC
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  // MARK: - IBOutlets
+  @IBOutlet var takePictureButton: UIButton!
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var progressView: UIProgressView!
+  @IBOutlet var activityIndicatorView: UIActivityIndicatorView!
+  
+  // MARK: - Properties
+  fileprivate var tags: [String]?
+  fileprivate var colors: [PhotoColor]?
+
+  // MARK: - View Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+      takePictureButton.setTitle("Select Photo", for: UIControlState())
+    }
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+
+    imageView.image = nil
+  }
+
+  // MARK: - Navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+    if segue.identifier == "ShowResults" {
+      guard let controller = segue.destination as? TagsColorsViewController else {
+        fatalError("Storyboard mis-configuration. Controller is not of expected type TagsColorsViewController")
+      }
+
+      controller.tags = tags
+      controller.colors = colors
+    }
+  }
+
+  // MARK: - IBActions
+  @IBAction func takePicture(_ sender: UIButton) {
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.allowsEditing = false
+
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      picker.sourceType = UIImagePickerControllerSourceType.camera
+    } else {
+      picker.sourceType = .photoLibrary
+      picker.modalPresentationStyle = .fullScreen
+    }
+
+    present(picker, animated: true, completion: nil)
+  }
+}
+
+// MARK: - UIImagePickerControllerDelegate
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else {
+      print("Info did not have the required UIImage for the Original Image")
+      dismiss(animated: true, completion: nil)
+      return
+    }
+    
+    imageView.image = image
+      
+    dismiss(animated: true, completion: nil)
+  }
+}
